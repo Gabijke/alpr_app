8453b389ec70a7ad106e00bb850a4b3e76b15d59
Maxim Isakov <85789260+Gabijke@users.noreply.github.com>
2022-08-03 13:13:40 +0500

    Update deskew_license_plate.py

--- deskew_license_plate.py
+++ deskew_license_plate.py
@@ -5,6 +5,12 @@ import numpy as np
 
 
 def rotate_image(image: np.array, angle: float) -> np.array:
+    
+    if len(src_img.shape) == 3:
+        h, w, _ = src_img.shape
+    else:
+        h, w = src_img.shape
+        
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
     result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
@@ -36,7 +42,3 @@ def compute_skew(src_img: np.array) -> float:
 
 def deskew_img(src_img: np.array):
     return rotate_image(src_img, compute_skew(src_img))
-
-
-
-
